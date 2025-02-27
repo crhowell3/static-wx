149ee43bf8372c7cb58592369458b1ea5abd03a7
Cameron <jango.djarin@proton.me>
2025-02-26 23:02:22 -0600

    Modified temperature and precipitation display in forecast infographic (#23)

--- app/components/WeeklyForecast.tsx
+++ app/components/WeeklyForecast.tsx
@@ -1,9 +1,9 @@
-import type { JSX } from "react";
-import type { ForecastData } from "../types/ForecastDataTypes";
-import ConditionIcon from "./ConditionIcon";
+import type { JSX } from 'react'
+import type { ForecastData } from '../types/ForecastDataTypes'
+import ConditionIcon from './ConditionIcon'
 
 interface ForecastDataProps {
-    forecastData: ForecastData[];
+    forecastData: ForecastData[]
 }
 
 const WeeklyForecast = ({ forecastData }: ForecastDataProps): JSX.Element => {
@@ -18,23 +18,30 @@ const WeeklyForecast = ({ forecastData }: ForecastDataProps): JSX.Element => {
                             (severe ? 'bg-red-200' : 'bg-white')
                         }
                     >
-                        <span className="font-bold text-lg text-black">
+                        <span
+                            className={
+                                'font-bold mb-4 text-lg ' +
+                                (day === 'Sat' || day === 'Sun'
+                                    ? 'text-red-400'
+                                    : 'text-black')
+                            }
+                        >
                             {day}
                         </span>
                         <ConditionIcon condition={condition} />
-                        <span className="text-md font-semibold mt-2 text-black">
+                        <span className="text-sm font-semibold mt-4 text-black">
                             {precip}%
                         </span>
                         <span className="text-xl font-semibold mt-2 text-black">
                             {high}°F
                         </span>
-                        <span className="text-xl font-semibold mt-2 text-black">
+                        <span className="text-l font-semibold text-gray-500">
                             {low}°F
                         </span>
                     </div>
                 )
             )}
-            </>
+        </>
     )
 }
 
